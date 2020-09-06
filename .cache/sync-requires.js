34e31a9b996c32603c152e593e75cd60578067f8
Nilesh Pant <nileshpant6969@gmail.com>
2020-09-06 19:53:20 +0530

    added a blog

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/media/nilesh/nilesh/nilesh-portfolio/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/media/nilesh/nilesh/nilesh-portfolio/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/media/nilesh/nilesh/nilesh-portfolio/src/pages/about.js"))),
   "component---src-pages-blog-js": hot(preferDefault(require("/media/nilesh/nilesh/nilesh-portfolio/src/pages/blog.js"))),
